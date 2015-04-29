cecf8fb3a7094798f076051bc5e4f6ea988d514f
radrohit <radrohit@gmail.com>
2015-04-29 19:31:03 -0400

    Committing intial files

---
+++ movie_list/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+# Create your models here.
+class Movie(models.Model):
+	name = models.CharField(max_length = 150)
+	release_date = models.DateField()
+	poster = models.ImageField(
+		default = "")
+	description = models.TextField()
+	rating = models.IntegerField(
+		validators = [
+		MaxValueValidator(10),
+		MinValueValidator(0)
+		]
+
+		) 
+	genre = models.ForeignKey('movie_site.Genre')
+
+	def __str__(self):
+		return self.name
+
+
+class Genre(models.Model):
+	genre_name = models.CharField(max_length = 100)
+
+
+
+

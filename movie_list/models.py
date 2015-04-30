276dc2a410febbf9fd7f161085df8a022eddd0a2
radrohit <radrohit@gmail.com>
2015-04-29 20:41:58 -0400

    Added MPAA Ratings

--- movie_list/models.py
+++ movie_list/models.py
@@ -2,11 +2,22 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Movie(models.Model):
-	name = models.CharField(max_length = 150)
+	MPAA_RATINGS = (
+		('G', 'General Audience'),
+		('PG', 'Parental Guidance'),
+		('PG-13', 'Parents Cautioned'),
+		('R','Restricted'),
+		('NC-17', "Above 17"),
+		('N/A',"Not Available"),
+		)
+
+	movie_name = models.CharField(max_length = 150)
 	release_date = models.DateField()
-	poster = models.ImageField(
+	movie_poster = models.ImageField(
 		default = "")
-	description = models.TextField()
+	movie_description = models.TextField()
+	mpaa_rating = models.CharField(max_length =5,
+		choices = MPAA_RATINGS, default = 'N/A')
 	rating = models.IntegerField(
 		validators = [
 		MaxValueValidator(10),

d936b7d4b4be8490a3c20ce29bf948883f5a62cf
radrohit <radrohit@gmail.com>
2015-05-01 17:57:12 -0400

    Urls to display image

--- movie_list/urls.py
+++ movie_list/urls.py
@@ -2,7 +2,17 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import patterns
 urlpatterns = [
 	url(r'^$',views.movie_display),
-	] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	] 
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^images/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )

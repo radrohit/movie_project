6620fa31602f78311e73b82d2acdbae891f0baee
radrohit <radrohit@gmail.com>
2015-05-01 13:09:54 -0400

    include directory for images

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -101,3 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),)
+ENV_PATH = os.path.abspath(BASE_DIR)
+MEDIA_ROOT = os.path.join(ENV_PATH, 'static','images')
+MEDIA_URL = 'images/'

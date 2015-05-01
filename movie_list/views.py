9af3faa17004c3737893fdd3298ce67c7ad19978
radrohit <radrohit@gmail.com>
2015-05-01 13:10:09 -0400

    Connecting database to template

--- movie_list/views.py
+++ movie_list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . models import Movie
 
 # Create your views here.
+def movie_display(request):
+	movies = Movie.objects.order_by('release_date')
+	return render(request,'movie_list/movie_display.html',{'movies':movies})
+

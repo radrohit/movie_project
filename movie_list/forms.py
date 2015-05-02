1a698b6819b011d302c007a27948d5fed5d19460
radrohit <radrohit@gmail.com>
2015-05-02 09:55:25 -0400

    Addedform for movie add

---
+++ movie_list/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Movie
+
+class AddMovie(forms.ModelForm):
+	class Meta:
+		model = Movie
+		fields = ['movie_name','release_date','movie_poster','movie_description','mpaa_rating','rating','genre_id']
